5a8fa28ef142d88148c3dbc72edf304e6028e98f
Erika Štefanková <e.stefankova15@gmail.com>
2021-02-22 17:47:46 +0100

    cache airline data from API

---
+++ airlines-app/src/app/services/data-provider.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable()
+export class DataProviderService {
+  private airlineDataStorageKey: string = 'airlinesData';
+  private lastUpdateStorageKey: string = 'lastUpdate';
+
+  constructor(private http: HttpClient) { }
+
+  getAirlinesOverviewData(): Subject<any> {
+    const sbj = new Subject<any>();
+
+    const data = localStorage.getItem(this.airlineDataStorageKey);
+    const lastUpdate = new Date(JSON.parse(localStorage.getItem(this.lastUpdateStorageKey)));
+    const actualDate = new Date();
+
+    // if there is cached data, which is not older than 12 hours, return it
+    // else get new one
+    if (data !== null && lastUpdate !== null && (Math.floor((actualDate.getTime() - lastUpdate.getTime()) / (1000*60*60))) < 12) {
+      console.log('returning cached data');
+      setTimeout(() => {
+        sbj.next(JSON.parse(localStorage.getItem(this.airlineDataStorageKey)));
+        sbj.complete();
+      });
+    }
+    else {
+      this.http.get('https://www.kayak.com/h/mobileapis/directory/airlines').subscribe((result) => {
+        const data = result;
+
+        // remove old data and save new
+        localStorage.removeItem(this.airlineDataStorageKey);
+        localStorage.setItem(this.airlineDataStorageKey, JSON.stringify(data));
+  
+        // remove information about last update and save new one
+        localStorage.removeItem(this.lastUpdateStorageKey);
+        localStorage.setItem(this.lastUpdateStorageKey, JSON.stringify(actualDate));
+        setTimeout(() => {
+          sbj.next(data);
+          sbj.complete();
+        });
+        
+        console.log('returning fresh data');
+      });
+    }
+
+    return sbj;
+  }
+  
+}
