e192455df55265cd91b783448e79471b3b499fac
Erika Štefanková <e.stefankova15@gmail.com>
2021-03-03 15:00:11 +0100

    simplified overview component, added types where needed

--- airlines-app/src/app/airlines-overview-module/airlines-overview.component.ts
+++ airlines-app/src/app/airlines-overview-module/airlines-overview.component.ts
@@ -1,87 +1,84 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
-import { DataProviderService } from '../services/data-provider.service';
+import { Component } from '@angular/core';
+
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { MenuItem } from 'primeng/api';
+
 import { AirlineDetailDialogComponent } from './airline-detail-dialog/airline-detail-dialog.component';
+import { DataProviderService } from '../services/data-provider.service';
 import { Airline } from './Airline';
 
 @Component({
     selector: 'airlines-overview-component',
     templateUrl: './airlines-overview.component.html',
 })
-export class AirlinesOverviewComponent implements OnInit {
-  public items: MenuItem[];
+export class AirlinesOverviewComponent {
+  public items: MenuItem[] = [
+    { label: 'Home', icon: 'pi pi-fw pi-home' },
+  ];
 
   // table settings
   public airlinesData: Airline[];
-  public columns: any;
   public tableLoading: boolean = false;
   public totalRecords: number;
   public first: number = 0;
   public showFavorites: boolean = false;
+  public columns: any[] =  [
+    { header: 'Logo', field: 'logoSrc', width: '10%' },
+    { header: 'Name', field: 'defaultName', width: '50%' },
+    { field: 'code', display: 'none' },
+  ];
 
   // general
   public title = 'Airlines overview';
-  private favorites: any[] = [];
-  private ref: DynamicDialogRef;
+  private favorites: string[] = [];
+  private dialogRef: DynamicDialogRef;
 
   constructor(
     private dataProviderService: DataProviderService,
     private dialogService: DialogService,
   ) { }
 
-  ngOnInit() {
-    this.items = [
-      { label: 'Home', icon: 'pi pi-fw pi-home' },
-    ]
-
-    this.columns = [
-      { header: 'Logo', field: 'logoSrc', width: '10%' },
-      { header: 'Name', field: 'defaultName', width: '50%' },
-      { field: 'code', display: 'none' },
-      { header: 'Code', field: 'code' } // tmp
-    ];
-  }
-
-  loadData($event: any) {
+  loadData($event: {first: number, rows: number}) {
     console.log('event', $event);
     this.tableLoading = true;
-    this.dataProviderService.getAirlinesOverviewData().subscribe((overviewData) => {
+    this.dataProviderService.getAirlinesOverviewData().subscribe((overviewData: Airline[]) => {
       this.airlinesData = overviewData.slice($event.first, $event.first + $event.rows);
       this.totalRecords = overviewData.length;
       this.tableLoading = false;
     });
+
     this.favorites = JSON.parse(localStorage.getItem('favorites'));
   }
 
   addToFavorites(code: string) {
     this.favorites = JSON.parse(localStorage.getItem('favorites'));
 
-    if (this.favorites === null) this.favorites = [];
+    if (this.favorites === null) 
+      this.favorites = [];
+
     this.favorites.push(code);
 
     localStorage.setItem('favorites', JSON.stringify(this.favorites));
   }
 
   showFavoriteAirlines($event: any) {
-    console.log('showFavoriteAirlines', $event);
     if ($event.checked) {
-      this.airlinesData = this.airlinesData.filter(airline => this.favorites.includes(airline.code));
+      this.airlinesData = this.airlinesData.filter((airline: Airline) => this.favorites.includes(airline.code));
       this.totalRecords = this.airlinesData.length;
 
       this.title = 'Favorite airlines overview';
     }
     else {
       this.loadData({first: 0, rows: 10});
+
       this.title = 'Airlines overview';
     }
   }
 
   showAirlineDetail(code: string) {
-    const airline = this.airlinesData.filter(a => a.code === code)[0];
-    let header = `${airline.defaultName}`;
-    this.ref = this.dialogService.open(AirlineDetailDialogComponent, {
-      header: header,
+    const airline = this.airlinesData.filter((airline: Airline) => airline.code === code)[0];
+    this.dialogRef = this.dialogService.open(AirlineDetailDialogComponent, {
+      header: airline.defaultName,
       width: '550px',
       data: code,
     });
